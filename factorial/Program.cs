e10f7fc59becb54e74d7fd9ba6819fb95aa6ef14
Magda <magdagvirjishvili@gmail.com>
2023-07-10 00:21:53 +0400

    magdas_comiit

---
+++ factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("factorial calculating");
+        Console.WriteLine("Enter the number");
+        int num = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            int counter = 1;
+
+            while (counter <= num)
+            {
+                factorial *= counter;
+                counter++;
+            }
+
+            Console.WriteLine("The factorial of " + num + " is: " + factorial);
+        }
+   
+}
